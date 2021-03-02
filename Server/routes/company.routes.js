a34e069436fd69cabaadbf3e532502f93f32ffee
Stephane <kalonjistephane@gmail.com>
2021-03-02 10:49:54 +0100

    CRUD- read: getAllCompanies

--- Server/routes/company.routes.js
+++ Server/routes/company.routes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const authCompanyController = require('../controllers/auth.companyController')
+const authCompanyController = require('../controllers/auth.companyController') //for signUp, login and signOut
+const companyController = require('../controllers/companyController') // read, update and delete
 
+// auth
 router.post("/register", authCompanyController.signUp);
+
+
+router.get("/", companyController.getAllCompanies);
 
 module.exports = router;

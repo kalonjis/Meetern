ecf1b035e4a0668b511086a44dbb419115de7ca2
Stephane <kalonjistephane@gmail.com>
2021-03-12 09:50:08 +0100

    logoutStudent ok

---
+++ client/src/components/home/HomeCompany.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeCompany = () =>{
+    return (
+        <section>
+            <div>hello depuis home compagny</div>
+            
+        </section>
+    )
+}
+export default HomeCompany

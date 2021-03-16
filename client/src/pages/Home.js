9001179f2665666b7df781056c7cd77aeaea4c74
Stephane <kalonjistephane@gmail.com>
2021-03-16 15:53:13 +0100

    No token + logs: new welcome message

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { UserContext } from '../components/AppContext';
 import HomeCompany from '../components/home/HomeCompany';
 import HomeStudent from '../components/home/HomeStudent';
+// import Profil from './Profil'
 
 const Home = () => {
   const user = useContext(UserContext)
@@ -13,7 +15,10 @@ const Home = () => {
           ):( user.type === "student" ? (
                 <HomeStudent/>
             ):(
-              <div>Hello! signUP man!</div>
+              <div>
+                <h1> Welcome on Meetern! </h1>
+                <Link to="/profil">join us now!</Link>
+              </div>
             )  
           )
         }

c6dbb461e68c545a05c705338df2a64f9521efce
Stephane <kalonjistephane@gmail.com>
2021-04-17 21:22:40 +0200

    display student profile in applications list

--- client/src/components/offers/companyOffers/ApplicationCard.js
+++ client/src/components/offers/companyOffers/ApplicationCard.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { timestampParser } from '../../utils';
+import StudentDetails from './StudentDetails';
 
 const ApplicationCard = ({application}) => {
     const students = useSelector( state => state.allStudentsReducer);
     const [student, setStudent]= useState([]);
-    
+    const [displayProfile, setDisplayProfile] = useState(false)
 
     useEffect(()=>{
         if( students[0]){
@@ -13,13 +14,27 @@ const ApplicationCard = ({application}) => {
         }
     },[students, application])
 
+    
+
     return (
-        <li>
-            <span> <b>Student</b> :{student.firstname + " "+ student.lastname} </span>
-            <span> <b>Statut</b> : {application.status} </span> 
-            <span> <b>Déposée le</b> : {timestampParser(application.timestamp)} </span>
-            <span> <button>Profil</button></span>
-        </li>
+        <>
+        { displayProfile === false &&(
+            <li>
+                <span> <b>Student</b> :{student.firstname + " "+ student.lastname} </span>
+                <span> <b>Statut</b> : {application.status} </span> 
+                <span> <b>Déposée le</b> : {timestampParser(application.timestamp)} </span>
+                <span> <button onClick={(e=>setDisplayProfile(true))}>Profil</button></span>
+            </li>
+            )
+        }
+        { displayProfile &&(
+            <>
+            <button onClick={(e=>setDisplayProfile(false))}> hide</button>
+            <StudentDetails student ={student} />
+            </>
+            )
+        }
+        </>
     )
 }
 
--- client/src/components/offers/companyOffers/StudentDetails.js
+++ client/src/components/offers/companyOffers/StudentDetails.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
-const StudentDetails = (id)=>{
-    const student = useSelector((state)=>state.detailsReducer);
+const StudentDetails = ({student})=>{
+    // const student = useSelector((state)=>state.detailsReducer);
     console.log(student)
 
     return (
         <div className="student-info" style={{border:  '2px solid blue', width:'30%'}} >
             <h2>{student.firstname +" "+ student.lastname}</h2>
-            <img src={student.picture} alt="student's pic" style={{minWidth:50, maxWidth:200, width:150, height:150, minHeight:50, maxHeight:200}}/>
+            <img src={student.picture} alt="student's pic" style={{width: "30px" , height: "30px"}}/>
             <p>Bio : {student.bio}</p>
             <p> internshipStart:  {student.internshipStart}</p>
             <p>internshipDuration: {student.internshipDuration}</p>

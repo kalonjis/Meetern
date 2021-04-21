5f00405a2e73ed4458427a0633c2ff6f113e9af4
Stephane <kalonjistephane@gmail.com>
2021-04-21 17:57:59 +0200

    useEffect to fetchOffers for message page

--- client/src/pages/Messages.js
+++ client/src/pages/Messages.js
@@ -1,9 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getAllOffers } from '../actions/allOffers.actions';
 import { UserContext } from '../components/AppContext';
 import Chatcompany from '../components/chat/company/Chat.company';
 
 const Messages = ()=> {
     const user = useContext(UserContext)
+    const [fetchData, setFetchData] = useState(true);
+    const dispatch = useDispatch();
+    
+    const fetchOffers = async()=>{
+      await dispatch(getAllOffers())
+      setFetchData(false)
+      console.log('offers refreshed')
+    }
+
+    useEffect(()=>{
+      if(fetchData){
+        fetchOffers()
+      }
+    })
   
     return (
       <div className="offers-page">

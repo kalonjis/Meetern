2fc947ef6ad21a6456b2d07653cd7b12c8af66da
Stephane <kalonjistephane@gmail.com>
2021-04-23 20:08:20 +0200

    offerCard :    timestampParser(offer.createdAt)}

--- client/src/components/offers/OfferCard.js
+++ client/src/components/offers/OfferCard.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { timestampParser } from '../utils';
+
 
 const OfferCard = ({offer}) => {
     
     return (
         <div className= "offerCard-container">
-            <h1>{offer.position} <span className="offerCard-offer-status">{offer.status}</span></h1>
+            <h1>{offer.position} <span className="offerCard-offer-status">{offer.status}</span>
+            </h1>
             <div className="offerCard-details">
                 <p>Description :  {offer.description} </p>
                 <p>Hiring possibility :  {offer.hiringPossibility} </p>
@@ -12,6 +15,7 @@ const OfferCard = ({offer}) => {
                 <p> Duration :  {offer.internshipDuration} </p>
                 <p>Place :  {offer.internshipPlace} </p>
                 <p>Face to face : {offer.faceToface} </p>
+                <div> Online since : { timestampParser(offer.createdAt)}</div>
             </div>
         </div>
     )
